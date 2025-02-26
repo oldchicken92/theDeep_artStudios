13b4a85192a9c571d0e0a6090df4837fb711dc03
oldchicken92e <a1926213@adelaide.edu.aum>
2025-02-26 21:06:52 +1030

    editing package.json file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/theDeep_artStudios',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
